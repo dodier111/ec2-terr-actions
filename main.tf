5e56c559269961cf6efa80e42defff57fe94acf7
dodier111 <65995124+dodier111@users.noreply.github.com>
2024-03-22 13:08:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,15 +3,15 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "s3buckkk"
-  acl    = "private"
+  bucket = "s3buq"
+  aws_s3_bucket_acl  = "private"
   region = "us-east-1"  
 
 }
 
 
 resource "aws_security_group" "example" {
-  name        = "newsecureeeety"
+  name        = "newsecureeeetyy"
   description = "Example security group for EC2 instance"
 
   ingress {
@@ -26,6 +26,6 @@ resource "aws_security_group" "example" {
 resource "aws_instance" "example" {
   ami           = "ami-0c101f26f147fa7fd"  
   instance_type = "t2.micro"
-  security_groups = ["newsecureeeety"] 
+  security_groups = ["newsecureeeetyy"] 
 
 }
